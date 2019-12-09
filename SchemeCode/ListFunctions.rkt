90c395eee9a10bd55e88c5efe7e5216be44e5f91
Hossein2x <hrakbari77@gmail.com>
2019-12-09 22:56:24 +0330

    new lines added

--- SchemeCode/ListFunctions.rkt
+++ SchemeCode/ListFunctions.rkt
@@ -1,11 +1,14 @@
 ;; As i said you can define function like this : '(2 3 4) or '(+ 2 3 4).
 ;; both of them disply a list as you see:
 (Display '(2 3 4))
+(newline)
 (Display '(+ 2 3 4))
 
 ;; Scheme has a ability to apply a function to a list.
 ;; for example :
+(newline)
 (Display (+ 2 3 4)) ; 2 + 3 + 4
+(newline)
 (Display (- 2 3 4)) ; 2 - 3 - 4
 
 ;; As you understood when u dont use " ' " next to a list it will take the first index
