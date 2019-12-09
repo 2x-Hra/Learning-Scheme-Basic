deca67ac95eb5c10967e2310f16c25a9d060bb58
Hossein2x <hrakbari77@gmail.com>
2019-12-09 22:54:14 +0330

    ListFunction added

---
+++ SchemeCode/ListFunctions.rkt
@@ -0,0 +1,13 @@
+;; As i said you can define function like this : '(2 3 4) or '(+ 2 3 4).
+;; both of them disply a list as you see:
+(Display '(2 3 4))
+(Display '(+ 2 3 4))
+
+;; Scheme has a ability to apply a function to a list.
+;; for example :
+(Display (+ 2 3 4)) ; 2 + 3 + 4
+(Display (- 2 3 4)) ; 2 - 3 - 4
+
+;; As you understood when u dont use " ' " next to a list it will take the first index
+; of a list as a Function and apply it to the list.
+;; it could be really useful in some situation like a get sum of elements of a list.
