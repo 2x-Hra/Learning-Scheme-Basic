e68ee1dda21fa24ac4b0876a20402327a64f7768
Hossein2x <hrakbari77@gmail.com>
2019-12-13 00:27:57 +0330

    useful function fixed

---
+++ SchemeCode/useful_built_inFunction.rkt
@@ -0,0 +1,7 @@
+;; there are some useful built in function in scheme which you should know
+
+;CAR : it will return the first index of array
+(car '(2 3 4))
+
+;CDR : it will return all the elements instead of first element
+(cdr '(2 3 4 5 ))
